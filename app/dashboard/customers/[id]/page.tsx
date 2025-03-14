b9613df543b0328218bc5033120f5f24c3acf56c
nguyenminhtriec <nmt@DeepLight>
2025-03-14 14:33:37 +0700

    NMT, 2025-3-14 T14:34

--- app/dashboard/customers/[id]/page.tsx
+++ app/dashboard/customers/[id]/page.tsx
@@ -2,13 +2,11 @@
 import { fetchPhotos, MarsPhoto } from "@/app/lib/actions";
 
 
-export  default async function Page(props: Promise<{id: string, img_src: string}>) {
-
-    const {id, img_src} = await props;
+export  default async function Page() {
 
     return (
         <div>
-            <img src={img_src} />
+            Expected Image
         </div>
     )
 }
